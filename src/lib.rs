39d0869122163da9b416b193b0531bce80e01777
Alekos Filini <alekos.filini@gmail.com>
2020-03-08 14:24:05 +0100

    Add some basic docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,28 @@
 #![allow(non_camel_case_types)]
 
+//! This library builds Tor and a minimal set of its dependencies into a single library that can be
+//! loaded like any other Rust crate dependency.
+//!
+//! The interface simply re-exports Tor's functions defined in its tor_api.h header.
+//!
+//! # Example
+//!
+//! ```no_run
+//! # use std::ffi::CString;
+//! # use tor_sys::*;
+//! unsafe {
+//!     let config = tor_main_configuration_new();
+//!     let argv = vec![
+//!         CString::new("tor").unwrap(),
+//!         CString::new("--version").unwrap(),
+//!     ];
+//!     let argv: Vec<_> = argv.iter().map(|s| s.as_ptr()).collect();
+//!     tor_main_configuration_set_command_line(config, argv.len() as i32, argv.as_ptr());
+//!
+//!     assert_eq!(tor_run_main(config), 0);
+//! }
+//! ```
+
 use std::os::raw::{c_char, c_int, c_void};
 
 type tor_main_configuration_t = c_void;
@@ -12,25 +35,4 @@ extern "C" {
         argv: *const *const c_char,
     ) -> c_int;
     pub fn tor_run_main(configuration: *const tor_main_configuration_t) -> c_int;
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::*;
-    use std::ffi::CString;
-
-    #[test]
-    fn test_start() {
-        unsafe {
-            let config = tor_main_configuration_new();
-            let argv = vec![
-                CString::new("tor").unwrap(),
-                CString::new("--version").unwrap(),
-            ];
-            let argv: Vec<_> = argv.iter().map(|s| s.as_ptr()).collect();
-            tor_main_configuration_set_command_line(config, argv.len() as i32, argv.as_ptr());
-
-            assert_eq!(tor_run_main(config), 0);
-        }
-    }
 }
